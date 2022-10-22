9d2f508ee9239ed077c650878a3efdfcf2c457bb
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-22 17:57:14 -0300

    :books: docs(jobs.read): add comment again

--- src/jobs.py
+++ src/jobs.py
@@ -4,6 +4,19 @@ from functools import lru_cache
 
 @lru_cache
 def read(path):
+    """Reads a file from a given path and returns its contents
+
+    Parameters
+    ----------
+    path : str
+        Full path to file
+
+    Returns
+    -------
+    list
+        List of rows as dicts
+    """
+
     with open(path, encoding="utf-8") as file:
         reader = csv.DictReader(file, delimiter=",", quotechar='"')
         docs_list = list(reader)
