83fb0f6ddae293529da1bee078cfeaffc7916110
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-22 18:19:41 -0300

    :sparkles: feat(insights): filter_by_industry

--- src/insights.py
+++ src/insights.py
@@ -82,7 +82,8 @@ def filter_by_industry(jobs, industry):
     list
         List of jobs with provided industry
     """
-    return []
+
+    return [job for job in jobs if job["industry"] == industry]
 
 
 def get_max_salary(path):
