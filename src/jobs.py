7db5b96f5b5dd68f0062e16bd5730f072a96d915
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-22 17:35:58 -0300

    :sparkles: feat: read function

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,11 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    with open(path, encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        docs_list = list(reader)
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    return docs_list
