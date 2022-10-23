ffced65adc8c9e102bbe853067e3d983b0541a30
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-22 21:25:04 -0300

    :tesT_tube: test(counter): create tests

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,29 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    file_path = "src/jobs.csv"
+
+    with open(file_path) as file:
+        read_data = file.read()
+
+        expected_results = {
+            "python": read_data.lower().lower().count("python"),
+            "javascript": read_data.lower().lower().count("javascript"),
+        }
+
+        words = [
+            "Python",
+            "PyThon",
+            "PYTHON",
+            "python",
+            "JavaScript",
+            "Javascript",
+            "JAVASCRIPT",
+            "javascript",
+        ]
+
+        for word in words:
+            assert expected_results[word.lower()] == count_ocurrences(
+                file_path, word
+            )
