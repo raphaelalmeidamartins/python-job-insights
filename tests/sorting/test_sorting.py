bc2e072633331b68848fbd7766e5a229249e765f
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-23 21:47:45 -0300

    :recycle: refactor(test): change some mock files directory

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -3,11 +3,11 @@ from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    jobs = read("tests/mocks/sort_jobs.csv")
+    jobs = read("tests/sorting/mocks/sort_jobs.csv")
     criterias = ["min_salary", "max_salary", "date_posted"]
 
     for criteria in criterias:
-        sorted_jobs = read(f"tests/mocks/sort_job_by_{criteria}.csv")
+        sorted_jobs = read(f"tests/sorting/mocks/sort_job_by_{criteria}.csv")
         sort_by(jobs, criteria)
 
         assert sorted_jobs == jobs
